f3265ddebde18497d7fcdebd7993e3979619d732
Nazar Komar <nazar@localhost.localdomain>
2021-01-24 19:14:17 +0200

    created gem

---
+++ lib/codebreaker.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "codebreaker/version"
+require 'codebreaker/game'
+
+module Codebreaker
+  class Error < StandardError; end
+  # Your code goes here...
+end
